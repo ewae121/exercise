8cbab1bdf0691d7624c728ca369a96e4e3c5c6f2
Bousquet <bousquet.laurent12@gmail.com>
2022-02-17 14:27:14 +0100

    feat: now we can use parameters

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from src.deduplicator import Deduplicator
 
+import argparse
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="String to deduplicate", type=str)
+    parser.add_argument("max_occurences", help="Maximum of duplicated occurences", type=int)
+    args = parser.parse_args()
+
     deduplicator  = Deduplicator()
-    deduplicated_string = deduplicator.deduplicate("abbbaaa", 2)
+    deduplicated_string = deduplicator.deduplicate(args.input, args.max_occurences)
     print("{}".format(deduplicated_string))
